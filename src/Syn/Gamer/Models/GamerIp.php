7153eab697e7a78943710fd135ea868a23163e6f
D. Klabbers <u@hyn.me>
2014-12-19 10:23:50 +0100

    preparations for finding relating users

--- src/Syn/Gamer/Models/GamerIp.php
+++ src/Syn/Gamer/Models/GamerIp.php
@@ -5,4 +5,34 @@ use Syn\Framework\Abstracts\Model;
 class GamerIp extends Model
 {
 	public $timestamps = false;
+
+	public function getDates()
+	{
+		return ['date_first_visit', 'date_last_visit'];
+	}
+
+	public function gamer()
+	{
+		return $this -> belongsTo('Syn\Gamer\Model\Gamer');
+	}
+
+	public function getRelatedAttribute()
+	{
+		$ip = $this -> ip;
+		$hostname = $this -> hostname;
+
+		return Gamer::where('id', '!=', $this->gamer_id)
+			->where(function($qwhere) use ($ip, $hostname)
+			{
+				$qwhere->whereHas('ips', function($q) use ($ip)
+				{
+					$q -> where('ip', $ip);
+				})
+				-> orWhereHas('ips', function($q) use ($hostname)
+				{
+					$q -> where('hostname', $hostname);
+				});
+			})
+			-> get();
+	}
 }
